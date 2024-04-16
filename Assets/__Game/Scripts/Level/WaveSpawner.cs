46b97f06e5197e49f78d4e11c2873c43dacddd1a
dageron93 <vladb93games@gmail.com>
2024-04-16 13:43:04 +0300

    Stop spawn if player dead

--- Assets/__Game/Scripts/Level/WaveSpawner.cs
+++ Assets/__Game/Scripts/Level/WaveSpawner.cs
@@ -17,22 +17,26 @@ namespace Assets.__Game.Scripts.Level
     private int _wavesPassed = 0;
     private readonly List<GameObject> _spawnedEnemies = new List<GameObject>();
     private int _previousRandomWave = -1;
+    private Coroutine _spawnRoutine;
 
+    private EventBinding<PlayerDead> _playerDeadEvent;
     private EventBinding<EnemyDead> _enemyDeathEvent;
 
     private void OnEnable()
     {
+      _playerDeadEvent = new EventBinding<PlayerDead>(StopSpawn);
       _enemyDeathEvent = new EventBinding<EnemyDead>(RemoveDeadEnemyFromList);
     }
 
     private void OnDisable()
     {
+      _enemyDeathEvent.Remove(StopSpawn);
       _enemyDeathEvent.Remove(RemoveDeadEnemyFromList);
     }
 
     private void Start()
     {
-      StartCoroutine(SpawnWaves());
+      _spawnRoutine = StartCoroutine(SpawnWaves());
     }
 
     private IEnumerator SpawnWaves()
@@ -124,6 +128,11 @@ namespace Assets.__Game.Scripts.Level
 
       if (_spawnedEnemies.Contains(deadEnemy))
         _spawnedEnemies.Remove(deadEnemy);
+    }
+
+    private void StopSpawn()
+    {
+      StopCoroutine(_spawnRoutine);
     }
   }
 }
